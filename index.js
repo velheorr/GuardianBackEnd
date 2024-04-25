69684de43fa3acc11b74f2ad92843c559be2c98a
Velheorr <velheorr@gmail.com>
2024-04-25 13:08:20 +0500

    current back

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-
 const express = require('express');
 require('dotenv').config()
 const axios = require('axios')
-var cors = require('cors')
+const cors = require('cors')
 
 const mongoose = require('mongoose');
 const {body, validationResult} = require('express-validator')
 
 
-const Card = require('./models/Card')
-const Order = require('./models/Order')
 const  User = require('./models/User')
 
-const {isLatLong} = require("validator");
 
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
+
 
 const start = async () => {
     try {
@@ -32,24 +29,8 @@ const start = async () => {
 start()
 
 
-app.get('/api/cards', async (req, res) => {
-    try {
-        const cards = await Card.find()
-        res.json(cards)
-    } catch (e) {
-        res.status(500).json({error: e.message})
-    }
-})
 
-app.get('/api/orders', async (req, res) => {
-    try {
-        const orders = await Order.find()
-        res.json(orders)
-    } catch (e) {
-        res.status(500).json({error: e.message})
-    }
-})
-
+/*DASHBOARD получить основные данные для графиков*/
 app.get('/api/dasahboarddata', async (req, res) => {
     try{
         const x = (await axios.get("https://mail.grdn.ru:777/upp_hs_ap/hs/v3/GetBlocSales")).data.response.data
@@ -60,69 +41,79 @@ app.get('/api/dasahboarddata', async (req, res) => {
     }
 })
 
-app.post('/api/order', body('name').isLength({min: 6}), body('phone').isNumeric(), async (req, res)=>{
-    try {
-        const errors = validationResult(req)
-        const {name, phone} = req.body
+/*IBOARD Получить данные блока реализация*/
+app.get('/api/iboardData', async (req, res) => {
+    try{
+        const x = (await axios.get("https://mail.grdn.ru:777/upp_hs_ap/hs/v3/GetSales")).data.response
+        const xData = await x.data
+        const xProjectTeam = await x.ProjectTeam
+        let arr = [];
 
-        const checkUniqueName = await Order.findOne({ name: name })
-
-        if (checkUniqueName){
-            res.status(409).json({ message: 'name already exist' })
-            return
+        const objectById = {};
+        for (const object of xProjectTeam) {
+            objectById[object.КодОбъекта] = object;
         }
 
-        if (!errors.isEmpty()){
-            return res.status(400).json({success: false, errors: errors.array()})
-        }
-
-        const order = new Order({name, phone})
-        await order.save()
-
-        res.json({order})
+        xData?.forEach(item => {
+            let newItem = item['КодОбъекта']
+            let x = Object.assign({}, item,objectById[newItem])
+            arr.push(x)
+        })
+        res.json(arr)
     }catch (e) {
-        res.status(500).send({message: e.message})
+        console.log(e)
     }
 })
 
-app.post('/api/register', body('login').isEmail(), body('password').isLength({min: '4'}), async (req, res)=>{
+/*Общая регистрация*/
+app.post('/api/register',
+    body('login').isEmail(),
+    body('password').isLength({min: '4'}),
+    body('name'),
+    async (req, res)=>{
     try {
         const errors = validationResult(req)
-        const {login, password} = req.body
+        const {login, password, name} = req.body
 
         const checkUniqueName = await User.findOne({ login: login })
 
         if (checkUniqueName){
-            res.status(409).json({ message: 'name already exist' })
-            return
+            return res.status(409).json({ message: 'name already exist' })
         }
-
         if (!errors.isEmpty()){
             return res.status(400).json({success: false, errors: errors.array()})
         }
 
-
-        const user = new User({login, password,  auth: {dashboard: false}})
+        const user = new User({login, password,name, auth: {dashboard: {login: false}, iboard: {login: false}}})
         await user.save()
 
-        res.json({user})
+        const result = {id: 200,message: "Пользователь успешно зарегистрирован", name: name, login: login}
+
+        /*res.json({user})*/
+        res.json({result})
     }catch (e) {
         res.status(500).send({message: e.message})
     }
 })
 
+/*Общая авторизация */
 app.post('/api/login',async (req, res)=>{
     try {
-        const {login, password} = req.body
-        let answer = true
-        const checkLogin = await User.findOne({'login': login, 'password': password, 'auth.dashboard': true})
-        if (!checkLogin){
-            answer = false
-            res.status(401).json({ message: 'login or password incorrect' })
-            return
+        const {login, password, from} = req.body
+
+        const checkLogin = await User.findOne({'login': login, 'password': password})
+        if (!checkLogin.auth[from].login){
+            return res.status(401).json({ message: 'У вас нет доступа для входа в систему' })
         }
-        res.json(answer)
+        if (checkLogin.auth[from].login){
+            const result = {message: "Авторизация успешна", name: checkLogin.name, login: login}
+            return res.json(result)
+        } else {
+            return res.status(401).json({ message: 'Неверный логин или пароль' })
+        }
     }catch (e) {
         res.status(500).send({message: e.message})
     }
 })
+
+
--- models/User.js
+++ models/User.js
@@ -1,10 +1,32 @@
 const {Schema, model} = require('mongoose')
 
 const user = new Schema({
-    login: {type: String, unique: true,required: true},
-    password: {type: String, required: true},
+    login: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        required: true,
+        type: String,
+    },
     auth: {
-        dashboard: {type: Boolean, default: false}
+        dashboard: {
+            login: {
+                type: Boolean,
+                default: false
+            },
+        },
+        iboard: {
+            login: {
+                type: Boolean,
+                default: false
+            },
+        }
     }
 })
 
